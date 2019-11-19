c6dc33f1885718f18b311a29ba1cf8000ba465a8
Mario_95 <mrojasc95.mr@gmail.com>
2019-11-19 18:39:13 -0500

    Spotify "constructor" defined
    requiring ('node-spotify-api)
    note: code importded from spotify API documentation

--- liri.js
+++ liri.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+// var spotify = keys.spotify;
 console.log(spotify);
