6f47676e34d799cfd7d061df2109398133d937d3
Mario_95 <mrojasc95.mr@gmail.com>
2019-11-20 16:27:06 -0500

    funtionality format redesigned
    main app funtionality replaced from switch statements to a single a funtion ran using swtich satemets.  individual commands defiend as funtions called in on the switch statement cases

--- liri.js
+++ liri.js
@@ -10,39 +10,63 @@ console.log(command);
 // define your query as the rest of the "process.arv" made into a string using the ".join" mehtod
 let query = process.argv.slice(3).join(" ");
 console.log(query)
-$(document).ready(function () {
 
-    if (command === "spotify-this-song") {
 
-        spotify.search({
-            type: 'track',
-            query: query,
-            limit: 5
-        }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-            console.log(data);
-            // view response objects and extract pertinent info 
-            // let artist = // response.artist
-            // let name = // response.songName
-            // let sample = //sample to song link
-            //  let album = // response.artist
-            //console.log(artist)
-            // console.log(name)
-            // console.log(sample)
-            // console.log(album)
-        });
+// $(document).ready(function () {
+
+function userInstruction(command, query) {
+
+    switch (command) {
+        case "spotify-this-song":
+            spotifySearch();
+            break;
+        case "concert-this":
+            concertSearch()
+            break;
+
+
+
 
     }
+}
 
-    if (command === "concert-this") {
-        let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response)
-        })
-    }
-})
+// define a sptify search function which uses the user query to return
+// infromation about a given track 
+let spotifySearch = () => {
+    spotify.search({
+        type: 'track',
+        query: query,
+        limit: 5
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data);
+        // view response objects and extract pertinent info 
+        // let artist = // response.artist
+        // let name = // response.songName
+        // let sample = //sample to song link
+        //  let album = // response.artist
+        //console.log(artist)
+        // console.log(name)
+        // console.log(sample)
+        // console.log(album)
+    });
+}
+// define a funtion to search for concerts using the user query
+let concertSearch = () => {
+
+    console.log("when is the concert?")
+    // let queryURL = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp"
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function (response) {
+    //     console.log(response)
+    // })
+}
+
+
+userInstruction(command, query);
+
+// })
