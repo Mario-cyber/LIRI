c1f3957b1fe19fb15748b44cb2af93170e7c1224
Mario_95 <mrojasc95.mr@gmail.com>
2019-11-20 14:36:14 -0500

    Spotify Almost functional
    Response from SPotify query obtained. Due to token restrcition, however, response object cannot be viewed and thus object info such as: artist, song name, and album cannot be extracted

--- liri.js
+++ liri.js
@@ -1,6 +1,48 @@
+// import all your requirements into your file:
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-// var spotify = keys.spotify;
-console.log(spotify);
+
+// define your "command" as the 0'th element of your trimmed "process.arv" object
+let command = process.argv.slice(2)[0];
+console.log(command);
+// define your query as the rest of the "process.arv" made into a string using the ".join" mehtod
+let query = process.argv.slice(3).join(" ");
+console.log(query)
+$(document).ready(function () {
+
+    if (command === "spotify-this-song") {
+
+        spotify.search({
+            type: 'track',
+            query: query,
+            limit: 5
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log(data);
+            // view response objects and extract pertinent info 
+            // let artist = // response.artist
+            // let name = // response.songName
+            // let sample = //sample to song link
+            //  let album = // response.artist
+            //console.log(artist)
+            // console.log(name)
+            // console.log(sample)
+            // console.log(album)
+        });
+
+    }
+
+    if (command === "concert-this") {
+        let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+        })
+    }
+})
